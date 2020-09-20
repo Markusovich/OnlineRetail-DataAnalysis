fd1bfc6b2f81d7f657066e65ac7402d2905220da
Simon Markus <52682568+Markusovich@users.noreply.github.com>
2020-09-19 20:35:08 -0500

    Add files via upload
    Revised entire project. Instead of doing everything from scratch, I was able to create a clean data table with only using the pandas library. Was able to make table for each customer and their purchase frequency, most recent purchase date, earliest purchase date, and total revenue spent. Removed missing values through list wise deletion, and removed negative values. Will show univariate and bivariate analysis soon.

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 import pandas as pd
-import datetime as dt
 
 # Import dataset into program
 data = pd.read_csv('Online Retail.csv', delimiter=',')
 
-# Removes a column from the dataset, or multiple columns
-removed_list = ['InvoiceNo', 'StockCode', 'Country']
-data.drop(removed_list, axis=1, inplace=True)
-
-# Removing rows with NaN values
+# Removing rows with NaN values, list wise deletion
 data.dropna(axis=0, inplace=True)
 
 # This functionality enables us to filter rows on specified conditions, removing rows not fit for analytics
+# Another for of list wise deletion
 data = data[(data['Quantity'] > 0)]
 data = data[(data['UnitPrice'] > 0)]
 
-# Reordering data columns, for easier reading
-data = data[['CustomerID', 'Description', 'Quantity', 'UnitPrice', 'InvoiceDate']]
-
-# We can tell how many unique customers have purchased from us
-# print(str(len(data['CustomerID'].unique())) + ' Customers')
-
+# Makes date more readable and program friendly
 data['InvoiceDate'] = pd.to_datetime(data.InvoiceDate)
 
-print(data.sample(20))
+# Creating new columns to apply functions on, needed for grouping step
+data['Total Revenue'] = data['Quantity'] * data['UnitPrice']
+data['Number Of Purchases'] = data['Quantity']
+data['Days From Last Purchase'] = data['InvoiceDate']
+data['Days From First Purchase'] = data['InvoiceDate']
+
+# Find most recently recorded date in dataframe
+column = data["Days From Last Purchase"]
+max_value = column.max()
+
+# Subtract each each date recorded from the most recent date recorded, to find number of days elapsed since then
+data['Days From Last Purchase'] = max_value - data['Days From Last Purchase']
+data['Days From First Purchase'] = max_value - data['Days From First Purchase']
+
+# For each customer, we get their unique statistics such as total revenue spent, total number of purchases,
+# most date of most recent purchase, and date of earliest purchase
+data = data.groupby(['CustomerID']).agg({'Number Of Purchases': ['sum'], 'Days From Last Purchase': ['min'],
+                                         'Days From First Purchase': ['max'], 'Total Revenue': ['sum']})
+
+# Randomizes rows so that they are not ordered by the customer ID numbers
+data = data.sample(frac=1).reset_index(drop=False)
 
 # Stores modified dataframe in new file
-data.to_csv('Clean Data.csv', index=False)
+data.to_csv('Clean Data.csv', index=True)
