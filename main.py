145ac9882b0b8d4ef1a5505189c6adf520636097
Simon Markus <52682568+Markusovich@users.noreply.github.com>
2020-10-12 03:28:49 -0500

    Add files via upload
    Added clustering.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from pandas_profiling import ProfileReport
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
 
 # Import dataset into program
 data = pd.read_csv('Online Retail.csv', delimiter=',')
@@ -41,6 +44,10 @@ data['Number Of Purchases'] = data['Number Of Purchases'].astype(np.int64)
 data['Days From Last Purchase'] = data['Days From Last Purchase'] / np.timedelta64(1, 'D')
 data['Days From First Purchase'] = data['Days From First Purchase'] / np.timedelta64(1, 'D')
 
+# Feature engineering, making graphs visualization easier to see
+#data['Total Revenue'] = np.log10(data['Total Revenue'])
+#data['Number Of Purchases'] = np.log10(data['Number Of Purchases'])
+
 # Randomizes rows so that they are not ordered by the customer ID numbers
 data = data.sample(frac=1).reset_index(drop=True)
 
@@ -51,5 +58,35 @@ data.columns = data.columns.droplevel(1)
 data.to_csv('Clean Data.csv', index=True)
 
 # Profile Report
-prof = ProfileReport(data)
-prof.to_file(output_file='output.html')
+#prof = ProfileReport(data)
+#prof.to_file(output_file='output.html')
+
+# Define number of clusters we want
+Kmean = KMeans(n_clusters=5)
+# We will find the number of clusters specified in a 4d space (4 variables)
+Kmean.fit(data)
+
+print('Mean of each cluster:')
+print(Kmean.cluster_centers_)
+
+# Testing clustering
+#print(Kmean.predict(np.array([173, 246, 295, 306]).reshape(1, -1)))
+#print(Kmean.predict(np.array([7436, 2, 357, 10674]).reshape(1, -1)))
+#print(Kmean.predict(np.array([236, 281, 281, 348]).reshape(1, -1)))
+
+# determine k using elbow method
+# k means determine k
+# ideal number of clusters is 5 according to the elbow of the graph
+distortions = []
+K = range(1, 10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k).fit(data)
+    kmeanModel.fit(data)
+    distortions.append(sum(np.min(cdist(data, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / data.shape[0])
+
+# Plot the elbow
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal k')
+plt.show()
