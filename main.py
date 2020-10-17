8c27a6f3abaeb84446eaca61137ffa895ed64607
Simon Markus <52682568+Markusovich@users.noreply.github.com>
2020-10-17 02:04:28 -0500

    Add files via upload
    Commented all code. Named and described all clusters.

--- main.py
+++ main.py
@@ -8,15 +8,12 @@ from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
 
 # Import dataset into program
+# dataframe will be designated as 'data'
 data = pd.read_csv('Online Retail.csv', delimiter=',')
 
 # Removing rows with NaN values, list wise deletion
+# In the near future I may consider a different approach to handling these missing values
 data.dropna(axis=0, inplace=True)
-
-# This functionality enables us to filter rows on specified conditions, removing rows not fit for analytics
-# Another for of list wise deletion
-data = data[(data['Quantity'] > 0)]
-data = data[(data['UnitPrice'] > 0)]
 
 # Makes date more readable and program friendly
 data['InvoiceDate'] = pd.to_datetime(data.InvoiceDate).dt.date
@@ -28,6 +25,7 @@ data['Days From Last Purchase'] = data['InvoiceDate']
 data['Days From First Purchase'] = data['InvoiceDate']
 
 # Find most recently recorded date in dataframe
+# This will be needed so that we can subtract a given date from it
 column = data["Days From Last Purchase"]
 max_value = column.max()
 
@@ -37,15 +35,25 @@ data['Days From First Purchase'] = max_value - data['Days From First Purchase']
 
 # For each customer, we get their unique statistics such as total revenue spent, total number of purchases,
 # most date of most recent purchase, and date of earliest purchase
+# This code makes a unique table for each customer id, finds the sum of all of their purchases, min days
+# from last purchase, max days from first purchase, and sum of all the revenue.
+# Thus, for each customer, their own unique row is created containing the features listed below.
 data = data.groupby(['CustomerID']).agg({'Number Of Purchases': ['sum'], 'Days From Last Purchase': ['min'],
                                          'Days From First Purchase': ['max'], 'Total Revenue': ['sum']})
 
 # Preforming manipulations on columns, for simplicity purposes
+# Making features easier to read
 data['Total Revenue'] = data['Total Revenue'].astype(np.int64)
 data['Number Of Purchases'] = data['Number Of Purchases'].astype(np.int64)
 data['Days From Last Purchase'] = data['Days From Last Purchase'] / np.timedelta64(1, 'D')
 data['Days From First Purchase'] = data['Days From First Purchase'] / np.timedelta64(1, 'D')
 
+# This code removes outliers that are 3 standard deviations away from the mean
+# z score is a value assigned to all data points to measure its distance from the mean
+# If the absolute value of the z score is 3 or greater, it is disregarded
+# I chose to implement this code because the extreme outliers were making my cluster means unrealistic
+# If I chose to keep outliers, I would have to consider 10 or more clusters in order to accurately group
+# all the data points. It would be a pain to find a unique name and description for that many clusters.
 z_scores = stats.zscore(data)
 abs_z_scores = np.abs(z_scores)
 filtered_entries = (abs_z_scores < 3).all(axis=1)
@@ -59,12 +67,16 @@ data.columns = data.columns.droplevel(1)
 
 # Define number of clusters we want
 # We will find the number of clusters specified in a 4d space (4 variables)
-Kmean = KMeans(n_clusters=3)
+Kmean = KMeans(n_clusters=4)
 # Cluster based on the select columns
+# fit function finds the clusters based on the dataframe argument
+# Here, I disregarded customer id as one of the variables to be considered,
+# because their value does not have a known connection to the customers shopping habits.
 Kmean.fit(data[['Number Of Purchases', 'Days From Last Purchase', 'Days From First Purchase', 'Total Revenue']])
 
 # Get centers for each cluster
 print('Mean of each cluster:')
+
 # This algorithm orders the clusters in such fashion that they are always in the same order every run
 for i in range(len(Kmean.cluster_centers_)):
     min_idx = i
@@ -73,25 +85,35 @@ for i in range(len(Kmean.cluster_centers_)):
             min_idx = j
     Kmean.cluster_centers_[i][3], Kmean.cluster_centers_[min_idx][3] = Kmean.cluster_centers_[min_idx][3], \
                                                                  Kmean.cluster_centers_[i][3]
+
 # Printing the array of each mean of each cluster
 print(Kmean.cluster_centers_)
-# Cluster 0: Low revenue
-# Cluster 1: Loyal customers
-# Cluster 2: High revenue
+# Cluster 0: Low spenders
+# Cluster 1: Most loyal customers, 2nd lowest spenders
+# Cluster 2: Above average high revenue
+# Cluster 3: Highest spenders, frequent customers
 
 # Find which cluster each customer belongs to based off select columns
+# Again, not including the customer id column because its numerical value has no say in the customers habits
+# Creates new column for each customer noting the cluster they belong to.
 data['Cluster Category'] = pd.Series(Kmean.predict(data[['Number Of Purchases',
                                                          'Days From Last Purchase',
                                                          'Days From First Purchase',
                                                          'Total Revenue']]._get_numeric_data().dropna(axis=1)),
                                      index=data.index)
 
+# Reordering of the columns for display
 data = data[['CustomerID',
              'Number Of Purchases',
              'Days From Last Purchase',
              'Days From First Purchase',
              'Total Revenue',
              'Cluster Category']]
+
+# Removes rows with negative stats
+# Does not seem normal for a customer to have a net negative number of purchases
+data = data[(data['Number Of Purchases'] > 0)]
+data = data[(data['Total Revenue'] > 0)]
 
 # Profile Report
 prof = ProfileReport(data)
@@ -100,12 +122,13 @@ prof.to_file(output_file='output.html')
 # Stores modified dataframe in new file
 data.to_csv('Clean Data.csv', index=True)
 
+# Code I stole. Finds optimal k using elbow method.
 # ############################## #
 # determine k using elbow method #
 # ############################## #
 # k means determine k
 distortions = []
-K = range(1, 9)
+K = range(1, 10)
 for k in K:
     kmeanModel = KMeans(n_clusters=k).fit(data[['Number Of Purchases', 'Days From Last Purchase',
                                                 'Days From First Purchase', 'Total Revenue']])
